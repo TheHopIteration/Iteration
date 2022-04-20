e39291500b7c29fb3c9830cd2dcbcb7800d32aac
Jonathan Luu <jonathanluu17@gmail.com>
2022-04-19 19:43:42 -0700

    fixed search bug

--- src/client/components/homepage/SearchBox.js
+++ src/client/components/homepage/SearchBox.js
@@ -125,7 +125,7 @@ export const SearchBox = ({ apiEvents, setApiEvents, setMapBase, mapRef, setCirc
               onFind();
             }}
             className="px-2 py-2 border-2 border-blue-400 text-white bg-blue-500 ml-1 text-sm font-semibold uppercase rounded hover:bg-black hover:bg-opacity-5 focus:outline-none focus:ring-0 transition duration-150 ease-in-out"
-            type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasExample" aria-controls="offcanvasExample"
+            type="button"  data-bs-target="#offcanvasExample" aria-controls="offcanvasExample"
           >
             <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth={2}>
             <path strokeLinecap="round" strokeLinejoin="round" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z" />
@@ -146,7 +146,7 @@ export const SearchBox = ({ apiEvents, setApiEvents, setMapBase, mapRef, setCirc
               onFind();
             }}
             className="px-4 py-2 border-2 border-blue-400 text-white bg-blue-500 ml-4 mr-4 text-sm font-semibold uppercase rounded hover:bg-black hover:bg-opacity-5 focus:outline-none focus:ring-0 transition duration-150 ease-in-out"
-            type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasExample" aria-controls="offcanvasExample"
+            type="button" data-bs-target="#offcanvasExample" aria-controls="offcanvasExample"
           >
             Search Events
           </button>
