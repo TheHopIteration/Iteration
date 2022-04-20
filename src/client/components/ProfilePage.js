d212997988087397bed26a21c6934ab4be7248fe
josh-merrell <jmerrell93@hotmail.com>
2022-04-19 21:18:43 -0600

    Adding new Friend works, issues with rendering updated list of friends

--- src/client/components/ProfilePage.js
+++ src/client/components/ProfilePage.js
@@ -1,15 +1,15 @@
 import React, { useState, useEffect } from 'react'
 import { Viewcomponent } from './profilepage/viewcomponent'
 import { Footer } from './Footer'
-import { Button, Stack } from '@mui/material'
+import { TextField, Button, Stack } from '@mui/material'
 
 
 export const ProfilePage = ({ user }) => {
+  const [userEvents, setUserEvents] = useState([]);
   const [eventsUserId, setEventsUserId] = useState([user.userid]);
   const [friends, setFriends] = useState([]);
   const [newFriend, setNewFriend] = useState('');
 
-  console.log('User Object: ', user)
   // view status to handle what view we're in
   const [view, setView] = useState('list')
   // handle button clicks
@@ -27,29 +27,35 @@ export const ProfilePage = ({ user }) => {
   
   
   
-  let backendUrl = new URL("http://localhost:3000/api/events");
+  let backendGetUserEventsUrl = new URL("http://localhost:3000/api/events");
   if (JSON.stringify(user) !== JSON.stringify({})) {
-    backendUrl.search = new URLSearchParams({ userid: user.userid }).toString();
+    backendGetUserEventsUrl.search = new URLSearchParams({ userid: user.userid }).toString();
   }
   
   let backendGetFriendsUrl = new URL("http://localhost:3000/api/friends");
-  if (JSON.stringify(user) !== JSON.stringify({})) {
-    backendGetFriendsUrl.search = new URLSearchParams({ userid: eventsUserId }).toString();
+  if (JSON.stringify(user) !== JSON.stringify('')) {
+    backendGetFriendsUrl.search = new URLSearchParams({ username: user.username }).toString();
   }
   
-  let backendNewFriendUrl = new URL("http://localhost:3000/api/friends");
-  if (JSON.stringify(user) !== JSON.stringify({})) {
-    backendNewFriendUrl.search = new URLSearchParams({ userA: user.username, userB: newFriend }).toString();
-  }
+  // let backendNewFriendUrl = new URL("http://localhost:3000/api/friends");
+  // if (JSON.stringify(user) !== JSON.stringify('')) {
+  //   backendNewFriendUrl.search = new URLSearchParams();
+  // }
   
-  const handleAddFriend = (e) => {
+  const handleAddFriend = () => {
     if (!newFriend) alert('Please enter a Friend\'s username before adding Friend!')
-  
-    fetch(backendNewFriendUrl, {
+    console.log('INVOKED');
+    console.log(`CURRENT newFriend: ${newFriend}, username: ${user.username}`)
+    fetch('http://localhost:3000/api/friends', {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json'
-      }
+      },
+      credentials: "include",
+      body: JSON.stringify({
+        "userA": user.username,
+        "userB": newFriend
+      })
     }).then(response => response.json())
       .then(data => {
         setFriends([...friends, data]);
@@ -60,7 +66,7 @@ export const ProfilePage = ({ user }) => {
   }
 
   useEffect(() => {
-    fetch(backendUrl, {
+    fetch(backendGetUserEventsUrl, {
       headers: {
         'Content-Type': 'application/json'
       }
@@ -75,33 +81,43 @@ export const ProfilePage = ({ user }) => {
     // re-render page when the apiEvents object.
   }, [JSON.stringify(userEvents)])
 
+  //load friends once upon loading
+  useEffect(() => {
+    fetch(backendGetFriendsUrl, {
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    }).then(response => response.json())
+      .then(data => {
+        let friends = [];
+        for (let friend of data) {
+          friends.push(friend['friend_b'])
+        }
+        setFriends([...friends]);
+      })
+      .catch(err => {
+        console.log(err);
+      })
+  }, []);
 
-  fetch(backendGetFriendsUrl, {
-    headers: {
-      'Content-Type': 'application/json'
-    }
-  }).then(response => response.json())
-    .then(data => {
-      console.log(`FRIENDS DATA: ${data}`);
-      setFriends(data);
-    })
-    .catch(err => {
-      console.log(err);
-    })
-
+  let friendDivs = [];
+  for (let i of friends) {
+    friendDivs.push(`<h1>${i}</h1>`);
+  }
 
   return (
     <div className='flex-col justify-center'>
       <div className="flex font-serif bg-gray-100 shadow-lg">
         <div className="h-[86vh] w-2/5 overflow-y-auto">
-          <h1>PLACEHOLDER</h1>
+          <div>Current Friends</div>
+          {friendDivs}
         </div>
         <div className="h-[86vh] w-3/5 overflow-y-auto justify-center">
           <Stack direction="row" spacing={2} justifyContent="center"
           sx = {{
             pt: 2,
           }}>
-            <TextField onChange={setNewFriend} label='friend username' variant='outlined'/>
+            <TextField onChange={(e) => setNewFriend(e.target.value)} label='friend username' variant='outlined'/>
             <Button
               variant='outlined'
               onClick={handleAddFriend}
--- src/server/controllers/friendController.js
+++ src/server/controllers/friendController.js
@@ -5,18 +5,18 @@ const friendController = {};
 friendController.getFriends = async (req, res, next) => {
     //get all fields for all friends where provided userID is the listed user_A
 
-    //req.body.userid must be present
     try {
-      const userid = req.query.userid;
+      const { username } = req.query;
       const sqlQuery = `
-        SELECT *
+        SELECT friend_b
         FROM friends AS f
-        LEFT JOIN users AS u f ON f.friendID = u.userid
-        WHERE user_A = $1
+        LEFT JOIN users AS u ON f.friend_a = u.username
+        WHERE friend_a = $1
       ;`;
 
-      const params = [userid];
+      const params = [username];
       const data = await db.query(sqlQuery, params);
+      console.log(`CURRENT FRIENDS OF ${username} ARE: ${JSON.stringify(data.rows)}`)
       res.locals.savedFriends = data.rows;
       return next();
 
@@ -32,15 +32,18 @@ friendController.getFriends = async (req, res, next) => {
 friendController.addFriend = async (req, res, next) => {
   //add a new friend as friendB where current user will be friendA. If requested friend username does not exist, return failure
 
+
   try {
     const { userA, userB } = req.body;
-
+    console.log(`BODY: ${JSON.stringify(req.body)}`);
     const sqlQuery = `
       INSERT INTO friends (friend_a, friend_b)
       VALUES ($1, $2) ON CONFLICT DO NOTHING
-    `
+    `;
     const params = [userA, userB];
+    if (!userA || !userB) console.log('NO USERS!')
     const data = await db.query(sqlQuery, params);
+    console.log(`RESULT: ${JSON.stringify(data.rows)}`)
     res.locals.newFriend = data.rows[0];
     return next();
 
